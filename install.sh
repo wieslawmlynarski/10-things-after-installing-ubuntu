b079ac17db05e0e05625c65c2b63481163ee1856
wmlynarski <wmlynarski>
2015-12-03 02:01:18 +0100

    inital commit.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository -y ppa:videolan/stable-daily
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
-sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+gksudo add-apt-repository -y ppa:videolan/stable-daily
+gksudo add-apt-repository -y ppa:webupd8team/java
+gksudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
+gksudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 
 echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
 echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
@@ -11,24 +11,24 @@ echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /
 wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc| sudo apt-key add -
 wget -qO - https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \ | sudo apt-key add -
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+gksudo apt-get update
+gksudo apt-get upgrade
+gksudo apt-get dist-upgrade
 
-sudo apt-get -y install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit oracle-java8-installer flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* ubuntu-make unity-tweak-tool gnome-session-fallback
+gksudo apt-get -y install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit oracle-java8-installer flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* ubuntu-make unity-tweak-tool gnome-session-fallback insync
 
 if [[ $(getconf LONG_BIT) = "64" ]]
 then
 	echo "64bit Detected" &&
 	echo "Installing Google Chrome" &&
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+	gksudo dpkg -i google-chrome-stable_current_amd64.deb &&
 	rm -f google-chrome-stable_current_amd64.deb
 else
 	echo "32bit Detected" &&
 	echo "Installing Google Chrome" &&
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
-	sudo dpkg -i google-chrome-stable_current_i386.deb &&
+	gksudo dpkg -i google-chrome-stable_current_i386.deb &&
 	rm -f google-chrome-stable_current_i386.deb
 fi
 
