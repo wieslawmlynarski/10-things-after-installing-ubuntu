bbe5e026159dd4c12646dcdd583bf18c0b1c3d2f
wmlynarski <wmlynarski>
2015-12-03 01:49:52 +0100

    inital commit.

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
 echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
 
-wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key add -
+wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc| sudo apt-key add -
+wget -qO - https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \ | sudo apt-key add -
 
 sudo apt-get update
 sudo apt-get upgrade
