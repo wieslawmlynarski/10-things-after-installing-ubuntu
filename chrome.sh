bb57ecaf1949bd738f343fe2c0ec6b53e5fbfebe
Wiesław Młynarski <wmlynarski@wmlynarski>
2016-08-15 23:03:09 +0200

    Fixing broken packages

--- chrome.sh
+++ chrome.sh
@@ -4,12 +4,12 @@ then
 	echo "64bit Detected" &&
 	echo "Installing Google Chrome" &&
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-	gksudo dpkg -i google-chrome-stable_current_amd64.deb &&
+	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
 	rm -f google-chrome-stable_current_amd64.deb
 else
 	echo "32bit Detected" &&
 	echo "Installing Google Chrome" &&
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
-	gksudo dpkg -i google-chrome-stable_current_i386.deb &&
+	sudo dpkg -i google-chrome-stable_current_i386.deb &&
 	rm -f google-chrome-stable_current_i386.deb
 fi
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-sudo apt-get -y install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit oracle-java8-installer flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* ubuntu-make unity-tweak-tool gnome-session-fallback insync
+sudo apt-get -y install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit oracle-java8-installer flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras
+sudo apt-get -y install ubuntu-make unity-tweak-tool
+sudo apt-get -y install gnome-session-flashback
+sudo apt-get -y install insync
 
 #create home app directories
 mdkir ~/APPS
